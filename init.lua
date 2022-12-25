3b81073c51b836ca127a9346ba5ff7efb7bbed69
andrew <andrew@node.rip>
2022-12-25 13:02:05 +0000

    Reconfiged LSP

--- init.lua
+++ init.lua
@@ -8,13 +8,14 @@ vim.opt.termguicolors = true
 vim.opt.smartindent = true
 vim.opt.guicursor = ''
 vim.opt.relativenumber = true
-
+vim.api.nvim_set_option("clipboard","unnamed")
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'morhetz/gruvbox'
     use 'junegunn/fzf'
     use 'nvim-lualine/lualine.nvim'
+
     require('lualine').setup {
 	options = {
 	    icons_enabled = true,
@@ -55,7 +56,9 @@ return require('packer').startup(function(use)
 	    inactive_winbar = {},
 	    extensions = {}
     }
+
     vim.cmd [[colorscheme gruvbox]]
+
     use 'nvim-treesitter/nvim-treesitter'
     require'nvim-treesitter.configs'.setup {
     ensure_installed = { "c", "lua", "rust" },
@@ -63,5 +66,26 @@ return require('packer').startup(function(use)
 	enable = true,
 	},
     }
-end)
 
+    use {
+	'VonHeikemen/lsp-zero.nvim',
+	requires = {
+	    -- LSP Support
+	    {'neovim/nvim-lspconfig'},
+	    {'williamboman/mason.nvim'},
+	    {'williamboman/mason-lspconfig.nvim'},
+
+	    -- Autocompletion
+	    {'hrsh7th/nvim-cmp'},
+	    {'hrsh7th/cmp-buffer'},
+	    {'hrsh7th/cmp-path'},
+	    {'saadparwaiz1/cmp_luasnip'},
+	    {'hrsh7th/cmp-nvim-lsp'},
+	    {'hrsh7th/cmp-nvim-lua'},
+
+	    -- Snippets
+	    {'L3MON4D3/LuaSnip'},
+	    {'rafamadriz/friendly-snippets'},
+	}
+    }
+end)
